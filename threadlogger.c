ed83754d8e0ef82a2a64290104166d332088ccf1
sandangl <emanuelepio.santangelo@studenti.unibas.it>
2023-01-18 20:42:03 +0100

    -- Fixed a nasty bug about strcat() use causing Segmentation Fault error.

--- threadlogger.c
+++ threadlogger.c
@@ -1,4 +1,9 @@
-#include <threadlogger.h>
+#include "threadlogger.h"
+
+
+void printer(char* msg){
+	printf("%s \n", msg);
+}
 
 char* getStatus(bool acquired){
 	if(acquired){
@@ -8,25 +13,23 @@ char* getStatus(bool acquired){
 }
 
 void printSmokingEvent(char* thread_name){
-	char msg[] = "";
-	strcat(msg, strcat(thread_name, " smoking..."));
+	char msg[255];
+	strcat(msg, thread_name);
+	strcat(msg, "smoking...");
 	printer(msg);
 }
 
 void printSemaphoreEvent(char* thread_name, char* sem_name, bool acquired){
-	char msg[] = "";
-	strcat(msg, strcat(thread_name, strcat(sem_name, getStatus(acquired))));
+	char msg[255];
+	strcat(msg, thread_name);
+	strcat(msg, getStatus(acquired));
+	strcat(msg, sem_name);
 	printer(msg);
 }
 
 void printTableClear(char* thread_name){
-	char msg[] = "";
-	strcat(msg, strcat(thread_name, strcat(" table ", CLEARED)));
+	char msg[255];
+	strcat(msg, thread_name);
+	strcat(msg, CLEARED);
 	printer(msg);
-}
-
-void printer(char* msg){
-	for (size_t i = 0; i < strlen(msg); i++) {
-       	printf("%c\n", i, msg[i]);
-  	}
 }
