302c939d18e4a559473cb0527c5625251d15ef79
sandangl <emanuelepio.santangelo@studenti.unibas.it>
2023-02-16 21:57:16 +0100

    Improved thread queue, fixing deadlock.

--- cigSmokers.c
+++ cigSmokers.c
@@ -21,99 +21,89 @@ worker smoker_paper;
 worker smoker_matches;
 
 void* agent_routine(void* arg){
-	int random = rand() % 3;
-	if(random == 0){
-		printf("AGENT: put paper on table.\nAGENT: put matches on table.\n");
-		sem_post(&paper);
-		sem_post(&matches);
+	while(1){
+		int random = rand() % 3;
+		if(random == 0){
+			printf("AGENT: put paper on table.\nAGENT: put matches on table.\n");
+			sem_post(&paper);
+			sem_post(&matches);
+		}
+		else if(random == 1){
+			printf("AGENT: put matches on table.\nAGENT: put tobacco on table.\n");
+			sem_post(&matches);
+			sem_post(&tobacco);
+		}
+		else{
+			printf("AGENT: put tobacco on table.\nAGENT: put paper on table.\n");
+			sem_post(&tobacco);
+			sem_post(&paper);
+		}
+		printf("AGENT: waiting for table to be cleared...\n");
+		sem_wait(&table_cleared);
 	}
-	else if(random == 1){
-		printf("AGENT: put matches on table.\nAGENT: put tobacco on table.\n");
-		sem_post(&matches);
-		sem_post(&tobacco);
-	}
-	else{
-		printf("AGENT: put tobacco on table.\nAGENT: put paper on table.\n");
-		sem_post(&tobacco);
-		sem_post(&paper);
-	}
-	pthread_mutex_lock(&queue_mutex);
-	enqueue(&waiting_threads, agent);
-	pthread_mutex_unlock(&queue_mutex);
-	printf("AGENT: waiting for table to be cleared...\n");
-	pthread_exit(NULL);
 }
 
 void* smoker_tobacco_routine(void* arg){
-	while(1){
-		printf("SMOKER_WITH_TOBACCO: waiting for paper...\n");
-		sem_wait(&paper);
-		printf("SMOKER_WITH_TOBACCO: took paper.\n");
-		printf("SMOKER_WITH_TOBACCO: waiting for matches...\n");
-		if(sem_trywait(&matches) == 0){
-			printf("SMOKER_WITH_TOBACCO: took matches.\n");
-			printf("SMOKER_WITH_TOBACCO: table cleared.\n");
-			sem_post(&table_cleared);
-			printf("SMOKER_WITH_TOBACCO: smoking...\n");
-			sleep(1);
-		}
-		else{
-			printf("SMOKER_WITH_TOBACCO: no matches, put back paper. I'll be back when table will be cleared.\n");
-			sem_post(&paper);
-			pthread_mutex_lock(&queue_mutex);
-			enqueue(&waiting_threads, smoker_tobacco);
-			pthread_mutex_unlock(&queue_mutex);
-			pthread_exit(NULL);
-		}
+	printf("SMOKER_WITH_TOBACCO: waiting for paper...\n");
+	sem_wait(&paper);
+	printf("SMOKER_WITH_TOBACCO: took paper.\n");
+	printf("SMOKER_WITH_TOBACCO: waiting for matches...\n");
+	if(sem_trywait(&matches) == 0){
+		printf("SMOKER_WITH_TOBACCO: took matches.\n");
+		printf("SMOKER_WITH_TOBACCO: table cleared.\n");
+		sem_post(&table_cleared);
+		printf("SMOKER_WITH_TOBACCO: smoking...\n");
 	}
+	else{
+		printf("SMOKER_WITH_TOBACCO: no matches, put back paper. I'll be back in a while.\n");
+		sem_post(&paper);
+	}
+	pthread_mutex_lock(&queue_mutex);
+	enqueue(&waiting_threads, smoker_tobacco);
+	pthread_mutex_unlock(&queue_mutex);
+	pthread_exit(NULL);
 }
 
 void* smoker_paper_routine(void* arg){
-	while(1){
-		printf("SMOKER_WITH_PAPER: waiting for tobacco...\n");
-		sem_wait(&tobacco);
-		printf("SMOKER_WITH_PAPER: took tobacco.\n");
-		printf("SMOKER_WITH_PAPER: waiting for matches...\n");
-		if(sem_trywait(&matches) == 0){
-			printf("SMOKER_WITH_PAPER: took matches.\n");
-			printf("SMOKER_WITH_PAPER: table cleared.\n");
-			sem_post(&table_cleared);
-			printf("SMOKER_WITH_PAPER: smoking...\n");
-			sleep(1);
-		}
-		else{
-			printf("SMOKER_WITH_PAPER: no matches, put back tobacco. I'll be back when table will be cleared.\n");
-			sem_post(&tobacco);
-			pthread_mutex_lock(&queue_mutex);
-			enqueue(&waiting_threads, smoker_paper);
-			pthread_mutex_unlock(&queue_mutex);
-			pthread_exit(NULL);
-		}
+	printf("SMOKER_WITH_PAPER: waiting for tobacco...\n");
+	sem_wait(&tobacco);
+	printf("SMOKER_WITH_PAPER: took tobacco.\n");
+	printf("SMOKER_WITH_PAPER: waiting for matches...\n");
+	if(sem_trywait(&matches) == 0){
+		printf("SMOKER_WITH_PAPER: took matches.\n");
+		printf("SMOKER_WITH_PAPER: table cleared.\n");
+		sem_post(&table_cleared);
+		printf("SMOKER_WITH_PAPER: smoking...\n");
 	}
+	else{
+		printf("SMOKER_WITH_PAPER: no matches, put back tobacco. I'll be back in a while.\n");
+		sem_post(&tobacco);
+	}
+	pthread_mutex_lock(&queue_mutex);
+	enqueue(&waiting_threads, smoker_paper);
+	pthread_mutex_unlock(&queue_mutex);
+	pthread_exit(NULL);
 }
 
 void* smoker_matches_routine(void* arg){
-	while(1){
-		printf("SMOKER_WITH_MATCHES: waiting for tobacco...\n");
-		sem_wait(&tobacco);
-		printf("SMOKER_WITH_MATCHES: took tobacco.\n");
-		printf("SMOKER_WITH_MATCHES: waiting for paper...\n");
-		if(sem_trywait(&paper) == 0){
-			printf("SMOKER_WITH_MATCHES: took paper.\n");
-			printf("SMOKER_WITH_MATCHES: table cleared.\n");
-			sem_post(&table_cleared);
-			printf("SMOKER_WITH_MATCHES: smoking...\n");
-			sleep(1);
-		}
-		else{
-			printf("SMOKER_WITH_MATCHES: no paper, put back tobacco. I'll be back when table will be cleared.\n");
-			sem_post(&tobacco);
-			pthread_mutex_lock(&queue_mutex);
-			enqueue(&waiting_threads, smoker_matches);
-			pthread_mutex_unlock(&queue_mutex);
-			pthread_exit(NULL);
-		}
+	printf("SMOKER_WITH_MATCHES: waiting for tobacco...\n");
+	sem_wait(&tobacco);
+	printf("SMOKER_WITH_MATCHES: took tobacco.\n");
+	printf("SMOKER_WITH_MATCHES: waiting for paper...\n");
+	if(sem_trywait(&paper) == 0){
+		printf("SMOKER_WITH_MATCHES: took paper.\n");
+		printf("SMOKER_WITH_MATCHES: table cleared.\n");
+		sem_post(&table_cleared);
+		printf("SMOKER_WITH_MATCHES: smoking...\n");
 	}
+	else{
+		printf("SMOKER_WITH_MATCHES: no paper, put back tobacco. I'll be back in a while.\n");
+		sem_post(&tobacco);
+	}
+	pthread_mutex_lock(&queue_mutex);
+	enqueue(&waiting_threads, smoker_matches);
+	pthread_mutex_unlock(&queue_mutex);
+	pthread_exit(NULL);
 }
 
 void new_detached_thread(pthread_t *t, void* (*routine) (void* arg)){
@@ -129,7 +119,7 @@ void smoking_hall(){
 			pthread_mutex_unlock(&queue_mutex);
 			new_detached_thread(&extracted.thread_id, extracted.routine);
 		}
-		sem_wait(&table_cleared);
+		sleep(1);
 	}
 }
 
